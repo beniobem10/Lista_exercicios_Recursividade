67af6c34f61989c331790db72ba9900d5bbc4f6c
beniobem10 <85081977+beniobem10@users.noreply.github.com>
2025-03-14 20:19:19 -0300

    Update main.c Exercicio 2 feito

--- main.c
+++ main.c
@@ -2,17 +2,31 @@
 #include <stdio.h>
 
 int fatorial(int nume);
+int natural(int numero);
 
 int main() {
     // Write C code here
     int re = fatorial(5);
     printf("%d", re);
+    
+     int ae = natural(3);
+    printf("\n %d", ae);
     return 0;
 }
 
+//exercicio 1
 int fatorial(int nume){
     if(nume <= 1){
         return 1;
     }
     return nume * fatorial(nume - 1) ;
+}
+
+//exercicio 2
+int natural(int numero){
+    if(numero <= 0){
+        return 0;
+    }
+   
+    return numero + natural(numero - 1); 
 }
