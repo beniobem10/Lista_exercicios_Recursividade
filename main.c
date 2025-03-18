2ef387a62e6d31807b3a4abb771f77ef175ea8d0
beniobem10 <85081977+beniobem10@users.noreply.github.com>
2025-03-17 23:20:11 -0300

    Update main.c exercicio 10 feito

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int natural(int numero); //exercicio 2
 int potencia(int numero,int elevado); //exercicio 4
 int Contagem(int numero);  //exercicio 5
 int digito(int numero); //exercicio 6
+int multirec(int numero,int multi); //exercicio 10
 int verificador (int vetor[], int pos, int comp); //exercicio 11
 
 int main() {
@@ -29,6 +30,10 @@ int main() {
         int digi = digito(123);
         printf("%d",digi);
 
+ //exercicio 10   
+int mulRec=multirec(7,3);
+printf("%d",mulRec);
+    
         //exercicio 11
         int vetor[5] = {3,3,3,3,4};
         printf("%d\n", verificador(vetor, 4, 3));
@@ -83,14 +88,25 @@ int Contagem(int numero){
             return salvo + digito(numero /10);
         }
 
-//exercicio 11
-int verificador(int vetor[], int pos, int comp){
-    if(pos < 0) return 0;
-    
-    int salvo = vetor[pos];
-
-    if( salvo == comp){
-        return verificador(vetor, pos -1, comp) + 1;
-    }
-    return verificador(vetor, pos -1, comp);
+//exercicio 10
+int multirec(int numero,int multi){
+        if(numero<=0 || multi<=0){
+            return 0;
+        }
+        if(multi<=0){
+            return 0;
+        }
+    return numero + multirec(numero,multi-1);
 }
+
+        //exercicio 11
+        int verificador(int vetor[], int pos, int comp){
+            if(pos < 0) return 0;
+            
+            int salvo = vetor[pos];
+        
+            if( salvo == comp){
+                return verificador(vetor, pos -1, comp) + 1;
+            }
+            return verificador(vetor, pos -1, comp);
+        }
